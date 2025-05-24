b3dd8d35c76453512fe1cdfe3054c399adc328ff
RamagiriVasanth <ramagirivasanth111@gmail.com>
2025-05-24 08:35:05 -0400

    Adding temporary changes to Github

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,13 +11,14 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if kwargs:
+        for key, value in kwargs.items():
+            params += key + "=" + value + "&"
 
     request_url = backend_url+endpoint+"?"+params
 
@@ -26,9 +27,10 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except requests.exceptions.RequestException as e:
         # If any error occurs
-        print("Network exception occurred")
+        print("Network exception occurred", e)
+
 
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
@@ -42,13 +44,15 @@ def analyze_review_sentiments(text):
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
+
+
 # def post_review(data_dict):
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
-        print("Network exception occurred")
+    except requests.exceptions.RequestException as e:
+        print("Network exception occurred", e)
 # Add code for posting review
