d3b5d34d5ee47a41da5ca133f273563320ff4e55
RamagiriVasanth <ramagirivasanth111@gmail.com>
2025-06-12 20:58:18 +0530

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -7,17 +7,13 @@ import PostReview from "./components/Dealers/PostReview"
 import { Routes, Route, Navigate } from 'react-router-dom';
 function App() {
   return (
-    <>
       <Routes>
-        <Route path="/" element={<Navigate to="/dealers" replace />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<LoginPanel />} />
         <Route path="/dealers" element={<Dealers/>} />
         <Route path="/dealer/:id" element={<Dealer/>} />
         <Route path="/postreview/:id" element={<PostReview/>} />
-        <Route path="*" element={<Navigate to="/dealers" replace />} />
       </Routes>
-    </>
   );
 }
 
