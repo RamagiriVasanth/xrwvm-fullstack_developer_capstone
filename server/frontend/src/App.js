b20e005a7d0e6ead1a6d7ced8a01817ddf729ece
RamagiriVasanth <ramagirivasanth111@gmail.com>
2025-06-12 18:20:07 +0530

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -5,6 +5,7 @@ import LoginPanel from './components/Login/Login';
 import Dealers from './components/Dealers/Dealers';
 import Dealer from "./components/Dealers/Dealer"
 import PostReview from "./components/Dealers/PostReview"
+import { Routes, Route, Navigate } from 'react-router-dom';
 function App() {
   return (
     <Routes>
